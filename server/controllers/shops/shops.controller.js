3949155335fd17c30b0c8882f6316a090698efb1
Matias <duarte.mati@gmail.com>
2020-01-22 12:20:07 -0300

    Skeleton of the project

---
+++ server/controllers/shops/shops.controller.js
@@ -0,0 +1,24 @@
+import Shops from '../../models';
+
+const mapShops = ({
+  name, logo, ratingScore, deliveryTimeMinMinutes,
+  deliveryTimeMaxMinutes, paymentMethodsList, cityName, area, discount,
+}) => ({
+  name,
+  logo: `https://d1v73nxuzaqxgd.cloudfront.net/restaurants/${logo}`,
+  rating: ratingScore,
+  deliveryTimeMinMinutes,
+  deliveryTimeMaxMinutes,
+  paymentMethods: paymentMethodsList.split(','),
+  city: cityName,
+  area,
+  discount,
+});
+
+export default {
+  getShops: (req, res) => {
+    const { query } = req.query;
+    const shops = Shops.search(query);
+    res.json({ shops: 'holisss' });
+  },
+};
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import routes from './controllers';
+import allowOrigin from './middleware/allow-origin';
+import bodyParser from './middleware/body-parser';
+
+const app = express();
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+if (isDevelopment) {
+  app.use(allowOrigin);
+}
+
+app.use(bodyParser);
+app.use(routes);
+
+function listen() {
+  const port = process.env.PORT || 8080;
+  return app.listen(port, () => {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(`Ready! Listening on port ${port}`);
+    }
+  });
+}
+
+export default () => listen();
